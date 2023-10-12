bba17ee55d7c3b9f23ca4149cb0167304d4cb67a
unknown <zohaibbhatti624@gmail.com>
2023-10-12 05:05:40 -0700

    new_paper

---
+++ logic.js
@@ -0,0 +1,19 @@
+const showNavBar = () => {
+    const shownavbar = document.getElementById('shownavbar');
+    shownavbar.style.display = "block";
+
+
+    
+}
+const hidenavbar = () => { 
+    // alert("zoahib");
+    const hidenavbar = document.getElementById('shownavbar');
+    hidenavbar.style.display="none"
+
+}
+const hideHumburger = () => {
+    alert("ali")
+    const hideHumburger = document.getElementById('hideHumburger')
+    hideHumburger.style.display = "none";
+
+}
